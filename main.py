4b89b67230bcb68653bc095d43946eb2795f366b
Kyle Padayachee <kyle44987@gmail.com>
2025-03-04 23:01:02 +0200

    api is completed

--- main.py
+++ main.py
@@ -1,13 +1,65 @@
 from langflow.load import run_flow_from_json
-TWEAKS = {
-  "TextInput-nnXnX": {
-      "input_value": "question"
+# .env
+from dotenv import load_dotenv
+import requests
+from typing import Optional
+import os
+
+load_dotenv()
+
+BASE_API_URL = "https://api.aiv2.dev/v1/ask"
+LANGFLOW_ID = "AskAIV2"
+APPLICATION_TOKEN = os.getenv("LANGFLOW_TOKEN") 
+
+def ask_aiv2(question, profile):
+ TWEAKS = {
+   "TextInput-nnXnX": {
+      "input_value": question
   },
   
   "TextInput-03p3z": {
-      "input_value": "profile"   
+      "input_value": profile  
   },
   
 }
 
 result = run_flow_from_json(flow = "AskAIV2.json",input_value="message",fallback_to_env_vars=False, tweaks=TWEAKS)
+
+def get_macros(goals, profile):
+ TWEAKS = {
+   "TextInput-nnXnX": {
+      "input_value": goals
+  },
+  
+   "TextInput-03p3z": {
+       "input_value": profile  
+    },
+  }
+ 
+ return run_flow_from_json("", tweaks=TWEAKS)
+ 
+def run_flow_from_json(message : str,
+                       output_type: str = "chat",
+                       input_type: str = "chat",
+                       tweaks: Optional[dict] = None,
+                       application_token: Optional[str] = None)-> dict:
+                       api_url = f"{BASE_API_URL}/{output_type}"
+                      
+                       payload = {
+                          "input_value": message,
+                          "output_type": output_type,
+                          "input_type": input_type,  
+
+                       }
+                       headers = None
+
+                       if tweaks:
+                           payload["tweaks"] = tweaks
+
+                       if application_token:
+                           headers = {"Authorization": f"Bearer {application_token}"}
+                       
+                       response = requests.request("POST", api_url, headers=headers, data=payload)
+                       return response.json() 
+result = get_macros("name: Kyle, age: 37","weight: 71kg, 1.72cm","muscle gain")
+print(result)
